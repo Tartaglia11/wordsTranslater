7fd6cf4a44c4a727bcbe8390776c88595bc577f3
Rinat Uzanzaev <rinatuzanzaev13@gmail.com>
2023-05-12 20:26:09 +0300

    created a first version of product

--- main.py
+++ main.py
@@ -10,13 +10,26 @@ def get_list_of_words(filename):
     return list_of_words
 
 
-def main():
-    lst = get_list_of_words('dictionary.txt')
+def select_words(filename):
+    lst = get_list_of_words(filename)
     index = random.randint(0, len(lst))
     random_word = lst[index]
     couple_of_words = random_word.split('-')
+
+    return couple_of_words
+
+
+def main():
+    couple_of_words = select_words('dictionary.txt')
     first_word = couple_of_words[0].strip()
     second_word = couple_of_words[1].strip()
+
+    print("Translated word: ", first_word)
+    translate = input("Enter a translation: ")
+    if translate == second_word:
+        print("It's correct!")
+    else:
+        print("You were make a mistake!")
 
 
 if __name__ == '__main__':
