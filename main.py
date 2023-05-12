843b182baa0c9170eadafaeabfb3d9ffc3924b36
Rinat Uzanzaev <rinatuzanzaev13@gmail.com>
2023-05-12 20:19:06 +0300

    getting a couple of words for translation

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+
+
 def get_list_of_words(filename):
     with open(filename, encoding="utf-8") as file:
         text = file.read()
@@ -8,7 +11,12 @@ def get_list_of_words(filename):
 
 
 def main():
-    print(get_list_of_words('dictionary.txt'))
+    lst = get_list_of_words('dictionary.txt')
+    index = random.randint(0, len(lst))
+    random_word = lst[index]
+    couple_of_words = random_word.split('-')
+    first_word = couple_of_words[0].strip()
+    second_word = couple_of_words[1].strip()
 
 
 if __name__ == '__main__':
